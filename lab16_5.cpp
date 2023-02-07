57a509a9c7eed32747d3dc8ac5d599ffca8dfb81
Homxy <120322865+Homxy@users.noreply.github.com>
2023-02-07 16:35:24 +0700

    Create lab16_5.cpp

---
+++ lab16_5.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+void shuffle(int *,int *,int *,int *); //Modify input arguments to pointer 
+
+int main(){
+	int a = 50, b = 100, c = 500, d = 1000;
+	
+	srand(time(0));	
+	
+	for(int i = 0;i < 10;i++){
+	    shuffle(&a,&b,&c,&d); //Modify input arguments to pointer 
+	    cout << a << " " << b << " " << c << " " << d << "\n";
+	}
+	
+	return 0;
+}
+
+//Write definition of shuffle() using pointer here 
+
+void shuffle(int *a,int *b,int *c,int *d){
+    int x[] ={*a,*b,*c,*d};
+    int r[] = {0,0,0,0};
+    for(int i=0;i<4;i++){
+        r[i]= rand()%4;
+        for(int j=0;j<i;j++){
+            if(r[i]==r[j]){
+                i--;
+                break;
+            }
+        }
+    }
+    *a = x[r[0]];
+    *b = x[r[1]];
+    *c = x[r[2]];
+    *d = x[r[3]];
+
+}
+
