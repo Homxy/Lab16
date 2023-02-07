e037445db1ae9f4b363bab0d69a99102a197301f
Homxy <120322865+Homxy@users.noreply.github.com>
2023-02-07 16:34:15 +0700

    Create lab16_4.cpp

---
+++ lab16_4.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+void shuffle(int &,int &,int &,int &);
+
+int main(){
+	int a = 50, b = 100, c = 500, d = 1000;
+	
+	srand(time(0));	
+	
+	for(int i = 0;i < 10;i++){
+	    shuffle(a,b,c,d);
+	    cout << a << " " << b << " " << c << " " << d << "\n";
+	}
+	
+	return 0;
+}
+void shuffle(int &a,int &b,int &c,int &d){
+    int x[] ={a,b,c,d};
+    int r[] = {0,0,0,0};
+    for(int i=0;i<4;i++){
+        r[i]= rand()%4;
+        for(int j=0;j<i;j++){
+            if(r[i]==r[j]){
+                i--;
+                break;
+            }
+        }
+    }
+    a = x[r[0]];
+    b = x[r[1]];
+    c = x[r[2]];
+    d = x[r[3]];
+
+}
+
